45045b94d6a0d68b351472971f0ad660b3405f14
EddyL <kvl192@hotmail.com>
2016-03-10 18:44:42 -0800

    Fixed the forloop according to proper levels

--- application/models/Timetable.php
+++ application/models/Timetable.php
@@ -6,49 +6,37 @@ class Timetable extends CI_Model {
     
     public function __construct() {
         parent::__construct();
-        $this->xml = simplexml_load_file(DATAPATH . 'master.xml');
-        
-        /// List day of week
-        foreach($this->xml->daysinweek->days as $days) {
-            $record = new stdClass();
-            $record->name = (string)$days['name'];
-            
-            $info_record = array();
-            $index = 0;
-            /// List of class info
-            foreach($days->info as $info) {
-                $record2 = new stdClass();
-                $record2->time = (string)$info->time; 
-                $record2->course_type = (string)$info->courseno['type'];
-                $record2->courseno = (string)$info->courseno;
-                $record2->instructor = (string)$info->instructor;
-                $record2->room = (string)$info->room;
-                
-                $info_record[$index++] = $record2;
-            }
-            $record->info = $info_record;
-            $this->days[$record->name] = $record;
-        }
+        $this->xml = simplexml_load_file(DATAPATH . 'day_of_week.xml');
         
         /// Getting data for days_of_week.xml
-        foreach($this->xml->daysinweek->days as $days) {
-            foreach($days->info as $info) {
-                
+        foreach($this->xml->daysinweek as $days_in_week) {
+            foreach($days_in_week->days as $days) {
+                foreach($days->info as $info) {
+                    
+                }
             }
         }
+        
+        $this->xml = simplexml_load_file(DATAPATH . 'class_period.xml');
         /// Getting data for class_period.xml
-        foreach($this->xml->classperiod->period as $period) {
-            foreach($period->info as $info) {
-                
+        foreach($this->xml->classperiod as $classperiod) {
+            foreach($classperiod->period as $period) {
+                foreach($period->info as $info) {
+                    
+                }
             }
         }
+        
+        $this->xml = simplexml_load_file(DATAPATH . 'course.xml');
         /// Getting data for course.xml
-        foreach($this->xml->courses->course as $course) {
-            foreach($course->info as $info) {
-                
+        foreach($this->xml->courses as $courses) {
+            foreach($courses->course as $couse) {
+                foreach($course->info as $info) {
+                    
+                }
             }
         }
-      
+        
     }
     
 }
